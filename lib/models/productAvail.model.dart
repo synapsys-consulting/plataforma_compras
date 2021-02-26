6bc362ae1c568e58b288b39c7cb3127a4a4dd535
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-02-26 18:50:28 +0100

    First delivery.

--- lib/models/productAvail.model.dart
+++ lib/models/productAvail.model.dart
@@ -3,52 +3,55 @@ import 'package:flutter/material.dart';
 
 class ProductAvail {
   ProductAvail({
-    @required this.product_id,
-    @required this.product_name,
-    @required this.product_description,
-    @required this.product_type,
+    @required this.productId,
+    @required this.productName,
+    @required this.productDescription,
+    @required this.productType,
     @required this.brand,
-    @required this.num_images,
-    @required this.num_videos,
+    @required this.numImages,
+    @required this.numVideos,
     @required this.avail,
-    @required this.product_price,
-    @required this.persone_id,
-    @required this.persone_name,
-    @required this.tax_id,
-    @required this.tax_apply,
+    @required this.purchased,
+    @required this.productPrice,
+    @required this.personeId,
+    @required this.personeName,
+    @required this.taxId,
+    @required this.taxApply,
     @required this.remark
   }
   );
-  final int product_id;
-  final String product_name;
-  final String product_description;
-  final String product_type;
+  final int productId;
+  final String productName;
+  final String productDescription;
+  final String productType;
   final String brand;
-  final int num_images;
-  final int num_videos;
+  final int numImages;
+  final int numVideos;
   int avail;
-  final double product_price;
-  final int persone_id;
-  final String persone_name;
-  final int tax_id;
-  final double tax_apply;
+  int purchased;
+  final double productPrice;
+  final int personeId;
+  final String personeName;
+  final int taxId;
+  final double taxApply;
   final String remark;
 
   factory ProductAvail.fromJson (Map<String, dynamic> json) {
     return ProductAvail (
-        product_id: int.parse(json['PRODUCT_ID'].toString()),
-        product_name: json['PRODUCT_NAME'],
-        product_description: json['PRODUCT_DESCRIPTION'] ?? '',
-        product_type: json['PRODUCT_TYPE'] ?? '',
+        productId: int.parse(json['PRODUCT_ID'].toString()),
+        productName: json['PRODUCT_NAME'],
+        productDescription: json['PRODUCT_DESCRIPTION'] ?? '',
+        productType: json['PRODUCT_TYPE'] ?? '',
         brand: json['BRAND'] ?? '',
-        num_images: int.parse((json['NUM_IMAGES'] ?? '0').toString()),
-        num_videos: int.parse((json['NUM_VIDEOS'] ?? '0').toString()),
+        numImages: int.parse((json['NUM_IMAGES'] ?? '0').toString()),
+        numVideos: int.parse((json['NUM_VIDEOS'] ?? '0').toString()),
         avail: int.parse((json['AVAIL'] ?? '0').toString()),
-        product_price: double.parse(json['PRODUCT_PRICE'].toString()),
-        persone_id: int.parse((json['PERSONE_ID'] ?? '0').toString()),
-        persone_name: json['PERSONE_NAME'] ?? '',
-        tax_id: int.parse(json['TAX_ID'].toString()),
-        tax_apply: double.parse(json['TAX_APPLY'].toString()),
+        purchased: 0,
+        productPrice: double.parse(json['PRODUCT_PRICE'].toString()),
+        personeId: int.parse((json['PERSONE_ID'] ?? '0').toString()),
+        personeName: json['PERSONE_NAME'] ?? '',
+        taxId: int.parse(json['TAX_ID'].toString()),
+        taxApply: double.parse(json['TAX_APPLY'].toString()),
         remark: json['REMARK'] ?? '',
     );
   }
