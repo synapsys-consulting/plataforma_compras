2b3e7e29052be5eca4b124f16d5d0e77c47b147e
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-02-26 18:50:14 +0100

    First delivery.

--- lib/models/cart.model.dart
+++ lib/models/cart.model.dart
@@ -10,47 +10,48 @@ class Cart with ChangeNotifier{
 
   void add (ProductAvail item){
     bool founded = false;
-    debugPrint('The value of founded is: ' + founded.toString());
-    debugPrint('The number of elements is: ' + this._items.length.toString());
-    this._items.forEach((element) {debugPrint('The element is: ' + element.product_id.toString());});
     if (this._items.length > 0) {
       this.items.forEach((element) {
-        if (element.product_id == item.product_id) {
-          element.avail += 1;
+        if (element.productId == item.productId) {
+          element.purchased += 1;
           founded = true;
         }
       });
     }
-    debugPrint('The value of founded after of the for: ' + founded.toString());
     if (!founded) {
-      item.avail = 1;
+      item.purchased = 1;
       _items.add(item);
     }
-    totalPrice = _items.fold(0, (total, current) => total + (current.product_price * current.avail));
+    totalPrice = _items.fold(0, (total, current) => total + (current.productPrice * current.purchased));
     notifyListeners();
   }
   void remove (ProductAvail item){
     bool founded = false;
     if (this._items.length > 0) {
-      this.items.forEach((element) { if (element.product_id == item.product_id) (element.avail == 1) ? founded = true : element.avail -= 1;});
+      this.items.forEach((element) { if (element.productId == item.productId) (element.purchased == 1) ? founded = true : element.purchased -= 1;});
     }
     if (founded) this._items.remove(item);
-    totalPrice = _items.fold(0, (total, current) => total + (current.product_price * current.avail));
+    totalPrice = _items.fold(0, (total, current) => total + (current.productPrice * current.purchased));
     notifyListeners();
   }
   void incrementAvail (ProductAvail item) {
-    this.items.forEach((element) { if (element.product_id == item.product_id) element.avail += 1;});
-    totalPrice = _items.fold(0, (total, current) => total + (current.product_price * current.avail));
+    this.items.forEach((element) { if (element.productId == item.productId) element.purchased += 1;});
+    totalPrice = _items.fold(0, (total, current) => total + (current.productPrice * current.purchased));
     notifyListeners();
   }
   void decrementAvail (ProductAvail item) {
-    this.items.forEach((element) { if (element.product_id == item.product_id) element.avail -= 1;});
-    totalPrice = _items.fold(0, (total, current) => total + (current.product_price * current.avail));
+    this.items.forEach((element) { if (element.productId == item.productId) element.purchased -= 1;});
+    totalPrice = _items.fold(0, (total, current) => total + (current.productPrice * current.purchased));
     notifyListeners();
   }
 
   ProductAvail  getItem (int index) {
     return _items[index];
+  }
+
+  void clearCart () {
+    this._items.clear();
+    notifyListeners();
   }
 
   int get numItems => this._items.length;
