a8e1e9c22c81082322f34a5b48d43763feded6d2
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-05-06 10:35:57 +0200

    Second version.

--- lib/models/cart.model.dart
+++ lib/models/cart.model.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:plataforma_compras/models/productAvail.model.dart';
 
-class Cart with ChangeNotifier{
+class Cart with ChangeNotifier {
   /// Internal, private state of the cart. Stores the ids of each item.
   final List<ProductAvail> _items = [];
-  double totalPrice= 0;
+  double totalPrice= 0.0;
 
   List<ProductAvail> get items => _items;
 
@@ -19,18 +19,47 @@ class Cart with ChangeNotifier{
       });
     }
     if (!founded) {
-      item.purchased = 1;
-      _items.add(item);
+      final itemCart = new ProductAvail(
+          productId: item.productId,
+          productName: item.productName,
+          productDescription: item.productDescription,
+          productType: item.productType,
+          brand: item.brand,
+          numImages: item.numImages,
+          numVideos: item.numVideos,
+          avail: item.avail,
+          purchased: 1,
+          productPrice: item.productPrice,
+          personeId: item.productId,
+          personeName: item.personeName,
+          taxId: item.taxId,
+          taxApply: item.taxApply,
+          remark: item.remark
+      );
+      _items.add(itemCart);
     }
     totalPrice = _items.fold(0, (total, current) => total + (current.productPrice * current.purchased));
     notifyListeners();
   }
-  void remove (ProductAvail item){
+  void remove (ProductAvail item) {
     bool founded = false;
+    ProductAvail tmpElement;
     if (this._items.length > 0) {
-      this.items.forEach((element) { if (element.productId == item.productId) (element.purchased == 1) ? founded = true : element.purchased -= 1;});
+      this._items.forEach((element) {
+        if (element.productId == item.productId) {
+          if (element.purchased == 1) {
+            founded = true;
+            tmpElement = element;
+          }
+          else {
+            element.purchased -= 1;
+          }
+        }
+      });
     }
-    if (founded) this._items.remove(item);
+    if (founded) {
+      this._items.remove(tmpElement);
+    }
     totalPrice = _items.fold(0, (total, current) => total + (current.productPrice * current.purchased));
     notifyListeners();
   }
@@ -45,12 +74,13 @@ class Cart with ChangeNotifier{
     notifyListeners();
   }
 
-  ProductAvail  getItem (int index) {
+  ProductAvail getItem (int index) {
     return _items[index];
   }
 
   void clearCart () {
     this._items.clear();
+    this.totalPrice = 0.0;
     notifyListeners();
   }
 
