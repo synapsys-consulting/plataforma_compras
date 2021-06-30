f36dd6d089376d5b0cae0a32414007fa60de3887
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-06-30 18:02:30 +0200

    First version.

---
+++ lib/models/addressGeoLocation.dart
@@ -0,0 +1,24 @@
+class AddressGeoLocation {
+  AddressGeoLocation ({
+    this.streetName,
+    this.streetNumber,
+    this.flatDoor,
+    this.postalCode,
+    this.locality,
+    this.province,
+    this.country,
+    this.state,
+    this.optional,
+  });
+  String streetName;
+  String streetNumber;
+  String flatDoor;
+  String postalCode;
+  String locality;
+  String province;
+  String country;
+  String state;
+  String optional;
+  String district;
+  String suburb;
+}
