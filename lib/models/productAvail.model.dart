db55ab929e4ef73bf837eb98f3393d2c2c1e86b6
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-06-30 18:03:40 +0200

    First version.

--- lib/models/productAvail.model.dart
+++ lib/models/productAvail.model.dart
@@ -15,9 +15,13 @@ class ProductAvail {
     @required this.productPrice,
     @required this.personeId,
     @required this.personeName,
+    @required this.businessName,
+    @required this.email,
     @required this.taxId,
     @required this.taxApply,
-    @required this.remark
+    @required this.idUnit,
+    @required this.remark,
+    @required this.minQuantitySell
   }
   );
   final int productId;
@@ -32,27 +36,35 @@ class ProductAvail {
   final double productPrice;
   final int personeId;
   final String personeName;
+  final String businessName;
+  final String email;
   final int taxId;
   final double taxApply;
+  final  String idUnit;
   final String remark;
+  int minQuantitySell;
 
   factory ProductAvail.fromJson (Map<String, dynamic> json) {
     return ProductAvail (
-        productId: int.parse(json['PRODUCT_ID'].toString()),
-        productName: json['PRODUCT_NAME'],
-        productDescription: json['PRODUCT_DESCRIPTION'] ?? '',
-        productType: json['PRODUCT_TYPE'] ?? '',
-        brand: json['BRAND'] ?? '',
-        numImages: int.parse((json['NUM_IMAGES'] ?? '0').toString()),
-        numVideos: int.parse((json['NUM_VIDEOS'] ?? '0').toString()),
-        avail: int.parse((json['AVAIL'] ?? '0').toString()),
-        purchased: 0,
-        productPrice: double.parse(json['PRODUCT_PRICE'].toString()),
-        personeId: int.parse((json['PERSONE_ID'] ?? '0').toString()),
-        personeName: json['PERSONE_NAME'] ?? '',
-        taxId: int.parse(json['TAX_ID'].toString()),
-        taxApply: double.parse(json['TAX_APPLY'].toString()),
-        remark: json['REMARK'] ?? '',
+      productId: int.parse(json['PRODUCT_ID'].toString()),
+      productName: json['PRODUCT_NAME'],
+      productDescription: json['PRODUCT_DESCRIPTION'] ?? '',
+      productType: json['PRODUCT_TYPE'] ?? '',
+      brand: json['BRAND'] ?? '',
+      numImages: int.parse((json['NUM_IMAGES'] ?? '0').toString()),
+      numVideos: int.parse((json['NUM_VIDEOS'] ?? '0').toString()),
+      avail: int.parse((json['AVAIL'] ?? '0').toString()),
+      purchased: 0,
+      productPrice: double.parse(json['PRODUCT_PRICE'].toString()),
+      personeId: int.parse((json['PERSONE_ID'] ?? '0').toString()),
+      personeName: json['PERSONE_NAME'] ?? '',
+      businessName: json['BUSINESS_NAME'].toString() ?? '',
+      email: json['EMAIL'] ?? '',
+      taxId: int.parse(json['TAX_ID'].toString()),
+      taxApply: double.parse(json['TAX_APPLY'].toString()),
+      idUnit: json['ID_UNIT'] ?? '',
+      remark: json['REMARK'] ?? '',
+      minQuantitySell: int.parse((json['MIN_QUANTITY_SELL'] ?? '0').toString())
     );
   }
 }
