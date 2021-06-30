eb7cd00da4f819cd9e0d98b9a8b61cc829e34d7f
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-06-30 18:01:33 +0200

    First version.

--- lib/utils/sizes.dart
+++ lib/utils/sizes.dart
@@ -1,9 +1,11 @@
 // Setting a proportion
+// With =
 // Height
 const double HeightInDpis_2 = 0.0025;
 const double HeightInDpis_4 = 0.0050;
 const double HeightInDpis_16 = 0.0200;
 const double HeightInDpis_20 = 0.0300;
+const double HeightInDpis_24 = 0.0443;
 const double HeightInDpis_35 = 0.0443;
 const double HeightInDpis_40 = 0.0507;
 const double HeightInDpis_60 = 0.0760;
