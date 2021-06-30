9e37a8d6d637da5a6ea2933c8f87fa4effecd93f
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-06-30 17:59:20 +0200

    First version.

--- lib/views/updateNameSurnameUser.view.dart
+++ lib/views/updateNameSurnameUser.view.dart
@@ -9,8 +9,10 @@ import 'package:plataforma_compras/utils/pleaseWaitWidget.dart';
 import 'package:plataforma_compras/utils/configuration.util.dart';
 import 'package:plataforma_compras/utils/responsiveWidget.dart';
 
-class _NameSurname {
+class _Name {
   String name;
+}
+class _Surname {
   String surNames;
 }
 class UpdateNameSurnameUser extends StatefulWidget {
@@ -26,7 +28,8 @@ class UpdateNameSurnameUser extends StatefulWidget {
 }
 class _UpdateNameSurnameUserState extends State<UpdateNameSurnameUser> {
   bool _pleaseWait = false;
-  _NameSurname _nameSurnameOut = new _NameSurname();
+  _Name _nameOut = new _Name();
+  _Surname _surnameOut = new _Surname();
   final PleaseWaitWidget _pleaseWaitWidget = PleaseWaitWidget(key: ObjectKey("pleaseWaitWidget"));
   String _firstNameIn;  // Save the name that come as the input parameter
   String _lastNameIn; // Save the surname that come as the input parameter
@@ -65,7 +68,6 @@ class _UpdateNameSurnameUserState extends State<UpdateNameSurnameUser> {
         ),
         onPressed: () async {
           try {
-            debugPrint ('He pinchado en el Guardar');
             _showPleaseWait (true);
             final Uri url = Uri.parse('$SERVER_IP/updateUserWithPersoneId/' + widget.userId.toString());
             final http.Response res = await http.put (
@@ -75,17 +77,19 @@ class _UpdateNameSurnameUserState extends State<UpdateNameSurnameUser> {
                   //'Authorization': jwt
                 },
                 body: jsonEncode (<String, String>{
-                  'user_lastname': _nameSurnameOut.surNames,
-                  'user_firstname': _nameSurnameOut.name,
+                  'user_lastname': _surnameOut.surNames,
+                  'user_firstname': _nameOut.name,
                   'gethash': 'true'
                 })
             ).timeout (TIMEOUT);
             if (res.statusCode == 200) {
               _showPleaseWait(false);
+              debugPrint ('Entro en el guardar por el 200');
               final String token = json.decode(res.body)['token'].toString();
               final SharedPreferences prefs = await _prefs;
               prefs.setString('token', token);
-              Navigator.popUntil(context, ModalRoute.withName('/'));
+              //Navigator.popUntil(context, ModalRoute.withName('/'));
+              Navigator.pop(context, _nameOut.name + ' ' + _surnameOut.surNames);
             } else {
               _showPleaseWait (false);
             }
@@ -126,16 +130,18 @@ class _UpdateNameSurnameUserState extends State<UpdateNameSurnameUser> {
         ],
       ),
       body: ResponsiveWidget (
-        smallScreen: _SmallScreenView (widget.firstName, widget.lastName, _nameSurnameOut),
+        smallScreen: _SmallScreenView (widget.firstName, widget.lastName, _nameOut, _surnameOut),
+        largeScreen: _LargeScreenView (widget.firstName, widget.lastName, _nameOut, _surnameOut),
       ),
     );
   }
 }
 class _SmallScreenView extends StatefulWidget {
-  _SmallScreenView (this.name, this.surNames, this.nameSurnameOut);
+  _SmallScreenView (this.name, this.surNames, this.nameOut, this.surnameOut);
   final String name;
   final String surNames;
-  final _NameSurname nameSurnameOut;
+  final _Name nameOut;
+  final _Surname surnameOut;
 
   @override
   _SmallScreenViewState createState() {
@@ -160,16 +166,18 @@ class _SmallScreenViewState extends State<_SmallScreenView> {
     } else {
       _surNamesController.text = widget.surNames;
     }
-    widget.nameSurnameOut.name = _nameController.text;
-    widget.nameSurnameOut.surNames = _surNamesController.text;
+    widget.nameOut.name = _nameController.text;
+    widget.surnameOut.surNames = _surNamesController.text;
+    _nameController.addListener(_onNameChanged);
+    _surNamesController.addListener(_onSurnamesChanged);
   }
   _onNameChanged(){
     debugPrint ('Antes de la asignación');
-    widget.nameSurnameOut.name = _nameController.text;
+    widget.nameOut.name = _nameController.text;
     debugPrint ('Después de la asignación');
   }
   _onSurnamesChanged(){
-    widget.nameSurnameOut.surNames = _surNamesController.text;
+    widget.surnameOut.surNames = _surNamesController.text;
   }
   @override
   void dispose() {
@@ -274,6 +282,164 @@ class _SmallScreenViewState extends State<_SmallScreenView> {
           ],
         ),
       )
+    );
+  }
+}
+class _LargeScreenView extends StatefulWidget {
+  _LargeScreenView (this.name, this.surNames, this.nameOut, this.surnameOut);
+  final String name;
+  final String surNames;
+  final _Name nameOut;
+  final _Surname surnameOut;
+
+  @override
+  _LargeScreenViewState createState() {
+    return _LargeScreenViewState();
+  }
+}
+class _LargeScreenViewState extends State<_LargeScreenView> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _surNamesController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.name == 'null') {
+      _nameController.text = '';
+    } else {
+      _nameController.text = widget.name;
+    }
+    if (widget.surNames == 'null') {
+      _surNamesController.text = '';
+    } else {
+      _surNamesController.text = widget.surNames;
+    }
+    widget.nameOut.name = _nameController.text;
+    widget.surnameOut.surNames = _surNamesController.text;
+    _nameController.addListener(_onNameChanged);
+    _surNamesController.addListener(_onSurnamesChanged);
+  }
+  _onNameChanged(){
+    debugPrint ('Antes de la asignación');
+    widget.nameOut.name = _nameController.text;
+    debugPrint ('Después de la asignación');
+  }
+  _onSurnamesChanged(){
+    widget.surnameOut.surNames = _surNamesController.text;
+  }
+  @override
+  void dispose() {
+    _nameController.removeListener(_onNameChanged);
+    _nameController.dispose();
+    _surNamesController.removeListener(_onSurnamesChanged);
+    _surNamesController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Row(
+          children: [
+            Flexible(child: Container ()),
+            Flexible(
+              flex: 2,
+              child: Center(
+                child: ListView(
+                  padding: EdgeInsets.all(20.0),
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          child: Text (
+                            'Nombre',
+                            style: TextStyle (
+                              fontWeight: FontWeight.w900,
+                              fontSize: 20.0,
+                              fontFamily: 'SF Pro Display',
+                              fontStyle: FontStyle.normal,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox (height: 15.0),
+                    Text(
+                      'Indícanos tu nombre y apellidos para localizarte en relación a tu pedido.',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontFamily: 'SF Pro Display',
+                        fontWeight: FontWeight.normal,
+                        color: tanteLadenOnPrimary,
+                      ),
+                      textAlign: TextAlign.justify,
+                      maxLines: 2,
+                      softWrap: true,
+                    ),
+                    SizedBox(height: 20.0),
+                    Form(
+                        autovalidateMode: AutovalidateMode.always,
+                        key: _formKey,
+                        child: Column (
+                          children: [
+                            TextFormField(
+                              controller: _nameController,
+                              decoration: InputDecoration (
+                                labelText: 'Nombre',
+                                labelStyle: TextStyle (
+                                  color: tanteLadenIconBrown,
+                                ),
+                                suffixIcon: IconButton (
+                                    icon: Icon(Icons.clear),
+                                    onPressed: () {
+                                      _nameController.clear();
+                                    }
+                                ),
+                              ),
+                              validator: (String value) {
+                                if (value.isEmpty) {
+                                  return 'Introduce un nombre válido';
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
+                            SizedBox (height: 15.0,),
+                            TextFormField(
+                              controller: _surNamesController,
+                              decoration: InputDecoration (
+                                labelText: 'Apellidos',
+                                labelStyle: TextStyle (
+                                  color: tanteLadenIconBrown,
+                                ),
+                                suffixIcon: IconButton (
+                                    icon: Icon(Icons.clear),
+                                    onPressed: () {
+                                      _surNamesController.clear();
+                                    }
+                                ),
+                              ),
+                              validator: (String value) {
+                                if (value == null) {
+                                  return 'Introduce apellidos válidos';
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
+                          ],
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(child: Container ())
+          ],
+        )
     );
   }
 }
