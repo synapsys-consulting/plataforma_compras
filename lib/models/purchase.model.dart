782b0f47d44997fc0ec46a42cea35e44c9fc596d
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-12-13 09:41:01 +0100

    Beta 2.

---
+++ lib/models/purchase.model.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:plataforma_compras/models/purchaseStatus.model.dart';
+
+class Purchase {
+  Purchase ({
+    @required this.orderId,
+    @required this.providerName,
+    @required this.buyerName,
+    @required this.showName,
+    @required this.allStatus,
+    @required this.statusId,
+    @required this.numStatus,
+    @required this.items,
+    @required this.situation,
+    @required this.totalAmount,
+    @required this.taxAmount,
+    @required this.discountAmount,
+    @required this.productPriceFinal,
+    @required this.productPrice,
+    @required this.totalBeforeDiscountWithoutTax,
+    @required this.totalAfterDiscountWithoutTax,
+    @required this.orderDate,
+    @required this.possibleStatusToTransitionTo
+  });
+  final int orderId;
+  final String providerName;
+  final String buyerName;
+  final String showName;
+  String allStatus;
+  String statusId;
+  int numStatus;
+  final int items;
+  final String situation;
+  final double totalAmount;
+  final double taxAmount;
+  final double discountAmount;
+  final double productPriceFinal;
+  final double productPrice;
+  final double totalBeforeDiscountWithoutTax;
+  final double totalAfterDiscountWithoutTax;
+  final DateTime orderDate;
+  List<PurchaseStatus> possibleStatusToTransitionTo;
+  factory Purchase.fromJson (Map<String, dynamic> json) {
+    final List<Map<String, dynamic>> resultListJson = json['STATUS_TO_TRANSITION_TO'].cast<Map<String, dynamic>>();
+    return new Purchase (
+        orderId: int.parse(json['ORDER_ID'].toString()),
+        providerName: json['PROVIDER_NAME'],
+        buyerName:  json['BUYER_NAME'],
+        showName: json['SHOW_NAME'],
+        allStatus: json['ALL_STATUS'],
+        statusId: json['STATUS_ID'],
+        numStatus: int.parse(json['NUM_STATUS'].toString()),
+        items: int.parse(json['ITEMS'].toString()),
+        situation: json['SITUACION'],
+        totalAmount: double.parse(json['TOTAL_AMOUNT'].toString()),
+        taxAmount: double.parse(json['TAX_AMOUNT'].toString()),
+        discountAmount: double.parse(json['DISCOUNT_AMOUNT'].toString()),
+        productPriceFinal: double.parse(json['PRODUCT_PRICE_FINAL'].toString()),
+        productPrice: double.parse(json['PRODUCT_PRICE'].toString()),
+        totalBeforeDiscountWithoutTax: double.parse(json['TOTAL_BEFORE_DISCOUNT_WITHOUT_TAX'].toString()),
+        totalAfterDiscountWithoutTax: double.parse(json['TOTAL_AFTER_DISCOUNT_WITHOUT_TAX'].toString()),
+        orderDate: DateTime.parse(json['ORDER_DATE']),
+        possibleStatusToTransitionTo: resultListJson.map<PurchaseStatus>((json) => PurchaseStatus.fromJson(json)).toList()
+    );
+  }
+}
