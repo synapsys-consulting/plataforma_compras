e951b74ea2dd449ba58f0ed48902296d881c51ad
Angel Ruiz Canton <aruizcanton@users.noreply.github.c>
2021-12-13 09:41:12 +0100

    Beta 2.

--- lib/models/purchaseLine.model.dart
+++ lib/models/purchaseLine.model.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:plataforma_compras/models/purchaseStatus.model.dart';
+
 class PurchaseLine {
   PurchaseLine ({
     @required this.orderId,
     @required this.providerName,
+    @required this.productId,
+    @required this.productName,
     @required this.allStatus,
+    @required this.statusId,
     @required this.numStatus,
+    @required this.banPrice,
+    @required this.banQuantity,
     @required this.items,
+    @required this.idUnit,
+    @required this.newQuantity,
+    @required this.newProductPrice,
+    @required this.banOfficialPrice,
     @required this.situation,
     @required this.totalAmount,
     @required this.taxAmount,
@@ -16,29 +27,54 @@ class PurchaseLine {
     @required this.productPrice,
     @required this.totalBeforeDiscountWithoutTax,
     @required this.totalAfterDiscountWithoutTax,
-    @required this.orderDate
+    @required this.orderDate,
+    @required this.remarkSeller,
+    @required this.remarkBuyer,
+    @required this.possibleStatusToTransitionTo
   });
   final int orderId;
   final String providerName;
-  final String allStatus;
+  final int productId;
+  final String productName;
+  String allStatus;
+  String statusId;
   final int numStatus;
-  final int items;
+  String banPrice;
+  String banQuantity;
+  int items;
+  String idUnit;
+  int newQuantity;
+  double newProductPrice;
+  String banOfficialPrice;
   final String situation;
-  final double totalAmount;
-  final double taxAmount;
-  final double discountAmount;
+  double totalAmount;
+  double taxAmount;
+  double discountAmount;
   final double productPriceFinal;
   final double productPrice;
-  final double totalBeforeDiscountWithoutTax;
-  final double totalAfterDiscountWithoutTax;
+  double totalBeforeDiscountWithoutTax;
+  double totalAfterDiscountWithoutTax;
   final DateTime orderDate;
+  String remarkSeller;
+  String remarkBuyer;
+  List<PurchaseStatus> possibleStatusToTransitionTo;
   factory PurchaseLine.fromJson (Map<String, dynamic> json) {
+    final List<Map<String, dynamic>> resultListJson = json['STATUS_TO_TRANSITION_TO'].cast<Map<String, dynamic>>();
     return new PurchaseLine (
       orderId: int.parse(json['ORDER_ID'].toString()),
       providerName: json['PROVIDER_NAME'],
+      productId: int.parse(json['PRODUCT_ID'].toString()),
+      productName: json['PRODUCT_NAME'],
       allStatus: json['ALL_STATUS'],
+      statusId: json['STATUS_ID'],
       numStatus: int.parse(json['NUM_STATUS'].toString()),
+      banPrice: json['BAN_PRICE'],
+      banQuantity: json['BAN_QUANTITY'],
       items: int.parse(json['ITEMS'].toString()),
+      idUnit: json['ID_UNIT'],
+      newQuantity: int.parse(json['NEW_QUANTITY'].toString()),
+      newProductPrice: double.parse(json['NEW_PRODUCT_PRICE_FINAL'].toString()),
+      banOfficialPrice: json['BAN_OFICIAL_PRICE'],
       situation: json['SITUACION'],
       totalAmount: double.parse(json['TOTAL_AMOUNT'].toString()),
       taxAmount: double.parse(json['TAX_AMOUNT'].toString()),
@@ -47,7 +83,10 @@ class PurchaseLine {
       productPrice: double.parse(json['PRODUCT_PRICE'].toString()),
       totalBeforeDiscountWithoutTax: double.parse(json['TOTAL_BEFORE_DISCOUNT_WITHOUT_TAX'].toString()),
       totalAfterDiscountWithoutTax: double.parse(json['TOTAL_AFTER_DISCOUNT_WITHOUT_TAX'].toString()),
-      orderDate: DateTime.parse(json['ORDER_DATE'])
+      orderDate: DateTime.parse(json['ORDER_DATE']),
+      remarkSeller: json['REMARK_SELLER'],
+      remarkBuyer: json['REMARK_BUYER'],
+      possibleStatusToTransitionTo: resultListJson.map<PurchaseStatus>((json) => PurchaseStatus.fromJson(json)).toList()
     );
   }
 }
